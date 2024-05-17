aafc9bfef569904a3ec16c74d1f7931d5614529e
Wes Higbee <wes.mcclure@gmail.com>
2024-05-17 11:50:48 -0500

    add initialForecasts - note there is a reason but I dont wanna say

--- api/Program.cs
+++ api/Program.cs
@@ -21,9 +21,19 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+// randomly generated, but a consistent set of forecasts for testing purposes
+var consistentForecasts = Enumerable.Range(1, 5).Select(index =>
+    new WeatherForecast
+    (
+        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        Random.Shared.Next(-20, 55),
+        summaries[Random.Shared.Next(summaries.Length)]
+    ))
+    .ToArray();
+
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
