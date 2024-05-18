5aade0dbc6a735bbcc055f239c99764ebeedd39d
Wes Higbee <wes.mcclure@gmail.com>
2024-05-17 22:54:56 -0500

    dump explainer

--- api/scan-codeql.sh
+++ api/scan-codeql.sh
@@ -5,3 +5,9 @@
 rm -rf codeql-db
 codeql database create  --language=csharp --source-root=./ codeql-db
 codeql database analyze codeql-db --format=csv --output=w00t.csv --rerun ~/repos/github/github/codeql/csharp/ql/src/Security\ Features/CWE-798/HardcodedCredentials.ql
+
+echo
+echo
+echo "## running query to dump explanation tables"
+echo
+codeql query run --database codeql-db ~/repos/github/github/codeql/csharp/ql/src/Security\ Features/CWE-798/HardcodedCredentials.ql
