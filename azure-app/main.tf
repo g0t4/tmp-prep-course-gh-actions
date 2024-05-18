0683b2ec184c1c482d0ff28f05360f2eea83fe12
Wes Higbee <wes.mcclure@gmail.com>
2024-05-18 01:14:59 -0500

    nuke tls example

--- azure-app/main.tf
+++ azure-app/main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 2.0.0"
     }
-    tls = {
-      source  = "hashicorp/tls"
-      version = ">= 4.0.0"
-    }
   }
 }
 
@@ -15,28 +11,12 @@ provider "azurerm" {
   features {}
 }
 
-provider "tls" {
-
-
+resource "azurerm_resource_group" "gh-actions" {
+  # az group list --output table
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
+  name     = "gh-actions"
+  location = "eastus"
 }
 
-
-resource "tls_private_key" "foo" {
-  # https://registry.terraform.io/providers/hashicorp/tls/latest/docs/resources/private_key
-  algorithm = "ED25519"
-  # terraform taint tls_private_key.foo  # invalidate generated key in local terraform state
-}
-
-# terraform output foo-id
-output "foo-id" {
-  value = tls_private_key.foo.id
-}
-output "foo-public" {
-  value = tls_private_key.foo.public_key_pem
-}
-output "foo-private" {
-  value     = tls_private_key.foo.private_key_pem
-  sensitive = true
-}
 
 // see private notes for setup before can use terraform
