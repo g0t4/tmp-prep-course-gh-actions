e2ba83e21f0ec636eed7f6cd9aa9714a7a373a05
Wes Higbee <wes.mcclure@gmail.com>
2024-05-18 19:06:45 -0500

    derp

--- api/smoke-test.sh
+++ api/smoke-test.sh
@@ -6,14 +6,14 @@
 
 # head_sha comes from workflow_run event payload, this is the version that was built/deployed
 head_sha=$1 # first arg is ${{ github.event.workflow_run.head_sha }}
-deployed_version=$(git describe --tags $head_sha)
-echo "Deployed version: $deployed_version"
+expected_version=$(git describe --tags $head_sha)
+echo "Expected version (was deployed): $expected_version"
 
 actual_version=$(curl -fsSL "http://gh-actions-web-api.azurewebsites.net/version")
 echo "Actual version: $actual_version"
 
 echo "Check if version matches:"
-if [ "$actual_version" != "$deployed_version" ]; then
+if [ "$actual_version" != "$expected_version" ]; then
   echo "  [FAIL] version mismatch"
   exit 1
 fi
